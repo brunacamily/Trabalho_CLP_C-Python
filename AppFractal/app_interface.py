bc8eae15dca93be10973a6aa03b676dad6044922
BrunaCamily <brunacamilydomingues@gmail.com>
2024-08-09 12:45:34 -0300

    inclui makefile e alguns ajustes no readme

--- AppFractal/app_interface.py
+++ AppFractal/app_interface.py
@@ -7,7 +7,7 @@ from matplotlib import colormaps
 import random
 
 # Carregar a biblioteca C
-mandelbrot = CDLL('./calculo.so')
+mandelbrot = CDLL('./calculo.dll')
 mandelbrot.generate_mandelbrot.argtypes = [c_int, c_int, c_double, c_double, c_double, c_double, c_int, POINTER(c_int)]
 
 cmap = 'hot'
